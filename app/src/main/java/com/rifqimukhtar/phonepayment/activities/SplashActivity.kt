c9a9ef520c6890347bb20a722db4b93b22f0ac9e
Oki Fauzan <oki.fauzan@gmail.com>
2020-05-12 13:52:43 +0700

    Add Login, Register, RegisterVerification, Splash Activity and Layout, also string

---
+++ app/src/main/java/com/rifqimukhtar/phonepayment/activities/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.rifqimukhtar.phonepayment.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.rifqimukhtar.phonepayment.R
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object{
+        private const val DURATION = 3000L
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, DURATION)
+    }
+}
