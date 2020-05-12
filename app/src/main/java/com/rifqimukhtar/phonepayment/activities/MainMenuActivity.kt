73f1c2cf1fad5024fc10f187f25ca712c8a1250e
Muhammad Rifqi Mukhtar <mukhtarrifqi@gmail.com>
2020-05-12 12:46:48 +0700

    Layout mainMenu and add string & dimen value

---
+++ app/src/main/java/com/rifqimukhtar/phonepayment/activities/MainMenuActivity.kt
@@ -0,0 +1,13 @@
+package com.rifqimukhtar.phonepayment.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rifqimukhtar.phonepayment.R
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
