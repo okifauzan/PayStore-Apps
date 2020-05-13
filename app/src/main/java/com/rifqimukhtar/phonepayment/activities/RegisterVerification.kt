d2562b8625ae5470560f05ae87fd70f02ea3de3c
Oki Fauzan <oki.fauzan@gmail.com>
2020-05-13 19:39:20 +0700

    Add timer and button resend for OTP

--- app/src/main/java/com/rifqimukhtar/phonepayment/activities/RegisterVerification.kt
+++ app/src/main/java/com/rifqimukhtar/phonepayment/activities/RegisterVerification.kt
@@ -1,8 +1,10 @@
 package com.rifqimukhtar.phonepayment.activities
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import com.rifqimukhtar.phonepayment.R
 import kotlinx.android.synthetic.main.activity_register_verification.*
@@ -14,6 +16,7 @@ class RegisterVerification : AppCompatActivity() {
     var postEmail: String? = null
     var postPhoneNumber: String? = null
     var postPassword: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_verification)
@@ -24,12 +27,32 @@ class RegisterVerification : AppCompatActivity() {
             postPhoneNumber = bundle?.getString("phoneNumber")
             postPassword = bundle?.getString("password")
         }
-
+        timerCount()
         onClickGroup()
+    }
+
+    fun timerCount(){
+        val timer = object : CountDownTimer(30000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                tvTimerOTP.setText("Wait ${millisUntilFinished/1000} seconds to resend code")
+                btnResend.isEnabled = false
+                btnResend.setTextColor(Color.RED)
+            }
+
+            override fun onFinish() {
+                btnResend.isEnabled = true
+                btnResend.setTextColor(Color.BLUE)
+            }
+        }.start()
     }
 
     fun onClickGroup() {
 
+        btnResend.setOnClickListener {
+            timerCount()
+            btnResend.isEnabled = false
+            btnResend.setTextColor(Color.RED)
+        }
         btnRegConfirmOTP.setOnClickListener {
             if (patternOTP.matches(etRegOTPNumber.text.toString())){
                 Log.d("test", "$postName, $postEmail, $postPhoneNumber, $postPassword")
