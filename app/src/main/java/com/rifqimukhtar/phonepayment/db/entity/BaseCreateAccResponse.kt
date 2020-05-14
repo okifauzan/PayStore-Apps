3b89e9b3c6730f61b7230476893747b1b445b70e
Oki Fauzan <oki.fauzan@gmail.com>
2020-05-14 16:16:17 +0700

    edit baseCreateAccResponse

--- app/src/main/java/com/rifqimukhtar/phonepayment/db/entity/BaseCreateAccResponse.kt
+++ app/src/main/java/com/rifqimukhtar/phonepayment/db/entity/BaseCreateAccResponse.kt
@@ -1,4 +1,11 @@
 package com.rifqimukhtar.phonepayment.db.entity
 
-class BaseCreateAccResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class BaseCreateAccResponse<T> (
+    @SerializedName("newUser")
+    var status: T,
+
+    @SerializedName("status")
+    var message: String
+)
