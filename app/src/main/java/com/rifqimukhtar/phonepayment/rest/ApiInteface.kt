a62c99bbb1fbbe8433b10a910bed0633c45e5e19
Oki Fauzan <oki.fauzan@gmail.com>
2020-05-14 11:26:06 +0700

    add @POST for otp and create Account

--- app/src/main/java/com/rifqimukhtar/phonepayment/rest/ApiInteface.kt
+++ app/src/main/java/com/rifqimukhtar/phonepayment/rest/ApiInteface.kt
@@ -1,7 +1,6 @@
 package com.rifqimukhtar.phonepayment.rest
 
-import com.rifqimukhtar.phonepayment.db.entity.Login
-import com.rifqimukhtar.phonepayment.db.entity.LoginResponse
+import com.rifqimukhtar.phonepayment.db.entity.*
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -10,4 +9,10 @@ import retrofit2.http.POST
 interface ApiInteface {
     @POST("login")
     fun postLogin(@Body login: Login): Call<LoginResponse>
+
+    @POST("otp")
+    fun postOTP(@Body otp: SendOTP): Call<SendOTPResponse>
+
+    @POST("user")
+    fun postRegister(@Body createAccount: CreateAccount): Call<CreateAccountResponse>
 }
