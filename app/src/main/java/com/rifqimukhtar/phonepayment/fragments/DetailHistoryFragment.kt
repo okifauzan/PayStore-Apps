344ebefa43b094e41b3272fb6a00cb29ad089390
Oki Fauzan <oki.fauzan@gmail.com>
2020-05-15 22:56:17 +0700

    add comment for adding status from list history fragment

--- app/src/main/java/com/rifqimukhtar/phonepayment/fragments/DetailHistoryFragment.kt
+++ app/src/main/java/com/rifqimukhtar/phonepayment/fragments/DetailHistoryFragment.kt
@@ -43,6 +43,7 @@ class DetailHistoryFragment : Fragment() {
 
         val adminFee = 0
 
+        tvSelectedStatusHistory //will be added after api have status response
         tvNamaHistory.text = history.name
         tvNominalHistory.text = history.amount.toString()
         tvAdminHistory.text = adminFee.toString()
